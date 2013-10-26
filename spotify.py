8c1cf4304eb4a051ef89b6f5f24469698e34b5b7
Sergio Ortega <LivingDeadDivision@gmail.com>
2013-10-26 17:17:17 +0000

    Try/except for catch problem with the strings encoding.

--- spotify.py
+++ spotify.py
@@ -12,24 +12,27 @@ class SpotifyClient:
 	def __init__(self,api_uri='http://ws.spotify.com/search/1/album'):
 		self._api_uri = api_uri
 		
-	def searchAlbum(self,album,artist,format='json'):	
-		if format == 'json':
-			print "Buscando: "+self.strip_accents(album)+" Artist: "+artist.split(",")[0]	
-			album = self.strip_accents(album).split("(")[0].split("-")[0].replace("&", "and").replace(" ", "%20")
-			# TODO: Find a proper way to different album names
-			if album == "Love%20Poems%20For%20Dying%20Children%20Act%20I%20":
-				album = "Love%20Poems%20For%20Dying%20Children%20Act%201%20"	
-			response = json.load(urllib2.urlopen(('http://ws.spotify.com/search/1/album.json?q='+album)))	
-							
-			for album in response['albums']:
-				artist = self.strip_accents(artist.split(",")[0].replace("&","and"))
-				albumArtist  = re.escape(album['artists'][0]['name'].split("-")[0].replace("&","and"))
-				if re.findall(artist, albumArtist, re.IGNORECASE):
-					print "Encontrado: "+album['name']
-					return album
-				elif re.findall(albumArtist, artist, re.IGNORECASE):
-					print "Encontrado: "+album['name']
-					return album
+	def searchAlbum(self,album,artist,format='json'):
+		try:	
+			if format == 'json':
+				print "Buscando: "+self.strip_accents(album)+" Artist: "+artist.split(",")[0]	
+				album = self.strip_accents(album).split("(")[0].split("-")[0].replace("&", "and").replace(" ", "%20")
+				# TODO: Find a proper way to different album names
+				if album == "Love%20Poems%20For%20Dying%20Children%20Act%20I%20":
+					album = "Love%20Poems%20For%20Dying%20Children%20Act%201%20"	
+				response = json.load(urllib2.urlopen(('http://ws.spotify.com/search/1/album.json?q='+album)))	
+								
+				for album in response['albums']:
+					artist = self.strip_accents(artist.split(",")[0].replace("&","and"))
+					albumArtist  = re.escape(album['artists'][0]['name'].split("-")[0].replace("&","and"))
+					if re.findall(artist, albumArtist, re.IGNORECASE):
+						print "Encontrado: "+album['name']
+						return album
+					elif re.findall(albumArtist, artist, re.IGNORECASE):
+						print "Encontrado: "+album['name']
+						return album
+		except UnicodeEncodeError:
+			print "Can´t encode the title of the album"
 			
 	def searchAlbumSongs(self,albumUri,format='json'):
 		#print 'http://ws.spotify.com/lookup/1/.json?uri='+albumUri+'&extras=track'
